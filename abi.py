53add3a600b9819051af8b155bc3f9ae3008fad1
Deniz Mert Edincik <deniz@edincik.com>
2024-10-10 14:49:32 +0200

    ABI generator
    still missing some pieces.

---
+++ abi.py
@@ -0,0 +1,138 @@
+import json
+import sys
+
+typeMap = {
+    "string": "String",
+    "bool": "Bool",
+    "uint": "UInt",
+    "uint8": "UInt8",
+    "uint16": "UInt16",
+    "uint32": "UInt32",
+    "uint64": "UInt64",
+    "uint128": "UInt128",
+    "uint256": "UInt256",
+    "int": "Int",
+    "int8": "Int8",
+    "int16": "Int16",
+    "int32": "Int32",
+    "int64": "Int64",
+    "int128": "Int128",
+    "int256": "Int256",
+    "address": "EVM.EVMAddress",
+}
+
+def mapType(t):
+    if t.endswith("[]"):
+        inner = t[:len(t)-2]
+        return "[" + mapType(inner) + "]"
+    return typeMap[t]
+
+class AbiFunction:
+
+    def __init__(self, function):
+        self.function = function
+        self.name = function["name"]
+        self.inputs = function["inputs"]
+        self.outputs = function["outputs"]
+        self.selector = self.functionSelector()
+        self.outputType = self.outputs[0]['type'] if len(self.outputs)>0 else None
+        self.cadenceOutputType = mapType(self.outputType) if self.outputType else None
+        self.type = function["type"]
+
+    def sanitizeFunctionArgName(self, name):
+        if name == "":
+            return "arg"
+        if name[0] == "_":
+            return self.sanitizeFunctionArgName(name[1:])
+        return name
+
+    def functionSelector(self):
+        types = [i["type"] for i in self.inputs]
+        return f"{self.name}({','.join(types)})"
+
+    def cadenceFunctionSignature(self) -> (str, str):
+        args = ", ".join(
+            [f"{self.sanitizeFunctionArgName(i['name'])}: {mapType(i['type'])}" for i in self.inputs]
+        )
+        output = ""
+        if self.cadenceOutputType:
+            output = f": {self.cadenceOutputType}?"
+        return (
+            f"access(EVM.Owner) fun {self.name}({args}){output}",
+            self.cadenceOutputType
+        )
+
+    def cadenceFunction(self):
+        signature, returnType = self.cadenceFunctionSignature()
+        cadenceReturnTypes = ",".join(
+            [f"Type<{mapType(i['type'])}>()" for i in self.outputs]
+        )
+        inputNames = [self.sanitizeFunctionArgName(i['name']) for i in function['inputs']]
+        if len(inputNames) > 0:
+            inputs = f"[{', '.join(inputNames)}]"
+        else:
+            inputs = "nil"
+
+        if self.cadenceOutputType:
+            return f"\t{signature} {{ \n\t\treturn self.call(\"{self.selector}\", [{cadenceReturnTypes}], {inputs}) as? {returnType}\n\t}}\n"
+        else:
+            return f"\t{signature} {{ \n\t\tself.call(\"{self.selector}\", [{cadenceReturnTypes}], {inputs})\n\t}}\n"
+
+
+if len(sys.argv) != 3:
+    print("Usage: python abi.py <abi.json> <name> [defaultAddress]")
+    sys.exit(1)
+
+contractName = sys.argv[2]
+defaultAddress = f"EVM.addressFromString({sys.argv[3]})" if len(sys.argv) == 4 else "base.address()"
+
+abi = json.loads(open(sys.argv[1]).read())
+
+functions  = ""
+for function in abi:
+    # only process functions
+    if function["type"] != "function":
+        continue
+    functions = f"{functions}{AbiFunction(function).cadenceFunction()}"
+
+print("""   access(all) attachment %s for EVM.CadenceOwnedAccount {
+        access(EVM.Owner) var contractAddress: EVM.EVMAddress
+        
+        access(EVM.Owner) fun call(
+          _ signature:String, 
+          _ returnTypes: [Type], 
+          _ data:[AnyStruct]?
+        ): AnyStruct{
+        
+          var data = EVM.encodeABIWithSignature(signature, data ??  [] as [AnyStruct])
+          var lastResult = base.call(
+            to: self.contractAddress,
+            data: data,
+            gasLimit: 15000000,
+            value: EVM.Balance(attoflow:0)
+          )
+          
+          if lastResult!.status != EVM.Status.successful{
+            return nil
+          }
+          
+          var res = EVM.decodeABI(types: returnTypes, data: lastResult!.data)
+          
+          if res.length==1{
+            return res[0]
+          }
+          return res
+        }
+        
+        access(EVM.Owner) fun setContractAddress(_ contractAddress: EVM.EVMAddress){
+          self.contractAddress = contractAddress    
+        }
+        
+        init(){
+          self.contractAddress = %s
+        }
+    
+// Generated functions
+%s
+    
+    }""" % (contractName, defaultAddress, functions))
